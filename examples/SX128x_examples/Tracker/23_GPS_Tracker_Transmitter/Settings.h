57a8b997acff8e458e5475d6d113128be5a08a04
LoRaSAT <stuart@robinsonemail.co.uk>
2020-03-22 14:06:10 +0000

    Add up time to tracker packet

--- examples/SX128x_examples/Tracker/23_GPS_Tracker_Transmitter/Settings.h
+++ examples/SX128x_examples/Tracker/23_GPS_Tracker_Transmitter/Settings.h
@@ -5,38 +5,38 @@
   suitable for the intended purpose and free from errors.
 *******************************************************************************************************/
 
-
 //*******  Setup hardware pin definitions here ! ***************
 
 //These are the pin definitions for one of my own boards, the Easy Mikrobus Pro Mini,
-//be sure to change the definitiosn to match your own setup. Some pins such as DIO1,
-//DIO2, BUZZER SWITCH1 may not be in used by this sketch so they do not need to be
-//connected and should be set to -1.
-
-//These are the pin definitions for one of my own boards, the Easy Mikrobus Pro Mini, 
 //be sure to change the definitiosn to match your own setup. 
 
 #define NSS 10                                  //select on LoRa device
 #define NRESET 9                                //reset on LoRa device
-#define RFBUSY 7                                //SX128X busy pin 
-#define DIO1 3                                  //DIO1 on LoRa device, used for RX and TX done 
+#define RFBUSY 7                                //SX126X busy pin 
+#define DIO1 3                                  //DIO1 on LoRa device, used for RX and TX done
 #define RX_EN -1                                //pin for RX enable, used on some SX1280 devices, set to -1 if not used
 #define TX_EN -1                                //pin for TX enable, used on some SX1280 devices, set to -1 if not used 
-#define LED1 8                                  //On board LED, high for on
 
-#define BUZZER -1                               //Buzzer if fitted, high for on. Set to -1 if not used      
+#define GPSPOWER 4                              //Pin that controls power to GPS, set to -1 if not used
+#define GPSONSTATE HIGH                         //logic level to turn GPS on via pin GPSPOWER 
+#define GPSOFFSTATE LOW                         //logic level to turn GPS off via pin GPSPOWER 
 
 #define RXpin A3                                //pin number for GPS RX input into Arduino - TX from GPS
 #define TXpin A2                                //pin number for GPS TX output from Arduino- RX into GPS
 
-#define GPSPOWER 4                              //Pin that controls power to GPS, set to -1 if not used
-#define GPSONSTATE HIGH                         //logic level to turn GPS on via pin GPSPOWER 
-#define GPSOFFSTATE LOW                         //logic level to turn GPS off via pin GPSPOWER
+#define LED1 8                                  //On board LED, high for on
+#define SupplyAD A7                             //pin for reading supply\battery voltage  
+#define BATVREADON 8                            //turns on battery resistor divider, high for on, -1 if not used
 
-#define LORA_DEVICE DEVICE_SX1280               //this is the device we are using
- 
+const float ADMultiplier = 10.0;                //multiplier for supply volts calculation
+#define DIODEMV 98                              //mV voltage drop accross diode at approx 8mA
 
-//*******  Setup LoRa Test Parameters Here ! ***************
+
+#define LORA_DEVICE DEVICE_SX1280    //we need to define the device we are using 
+
+
+
+//*******  Setup LoRa Parameters Here ! ***************
 
 //LoRa Modem Parameters
 const uint32_t Frequency = 2445000000;           //frequency of transmissions
@@ -44,6 +44,10 @@ const int32_t Offset = 0;                        //offset frequency for calibrat
 const uint8_t Bandwidth = LORA_BW_0200;          //LoRa bandwidth
 const uint8_t SpreadingFactor = LORA_SF12;       //LoRa spreading factor
 const uint8_t CodeRate = LORA_CR_4_5;            //LoRa coding rate
+
+const uint8_t TXpower = 10;                      //Power for transmissions in dBm
+
+#define ThisNode '2'                             //a character that identifies this tracker
 
 //**************************************************************************************************
 // GPS Settings
@@ -53,11 +57,11 @@ const uint8_t CodeRate = LORA_CR_4_5;            //LoRa coding rate
 //#define HardwareSerialPort Serial1             //if using hardware serial enable this define for hardware serial port 
 
 #define GPSBaud 9600                             //GPS Baud rate   
-#define WaitGPSFixSeconds 30                     //time to wait for a new GPS fix 
-#define echomS 2000                              //number of mS to run GPS echo for at startup    
 
-#define NoRXGPSfixms 15000                       //max number of mS to allow before no local fix flagged 
-#define DisplayRate 7                            //when working OK the GPS will get a new fix every second or so
-                                                 //this rate defines how often the display should be updated
+#define WaitGPSFixSeconds 30                     //time in seconds to wait for a new GPS fix 
+#define WaitFirstGPSFixSeconds 1800              //time to seconds to wait for the first GPS fix at startup
+#define Sleepsecs 5                              //seconds between transmissions, this delay is used to set overall transmission cycle time
+
+#define echomS 2000                              //number of mS to run GPS echo at startup    
 
 
