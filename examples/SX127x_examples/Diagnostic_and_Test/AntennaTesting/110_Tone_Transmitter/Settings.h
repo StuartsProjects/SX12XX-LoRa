27faff4c2c5914d061aa727e239962f2e7f7ba70
StuartsProjects <stuart [at] stuartsprojects.com>
2022-12-14 16:55:27 +0000

    Update examples

--- examples/SX127x_examples/Diagnostic_and_Test/AntennaTesting/110_Tone_Transmitter/Settings.h
+++ examples/SX127x_examples/Diagnostic_and_Test/AntennaTesting/110_Tone_Transmitter/Settings.h
@@ -7,32 +7,26 @@
 
 //*******  Setup hardware pin definitions here ! ***************
 
-//These are the pin definitions for one of my own boards based on a ATMega328 ProMini
+//These are the pin definitions for one of my own boards, the Easy Pro Mini,
 //be sure to change the definitions to match your own setup.
 
 #define NSS 10                                  //select pin on LoRa device
 #define NRESET 9                                //reset pin on LoRa device
 #define LED1 8                                  //on board LED, high for on
 #define DIO0 3                                  //DIO0 pin on LoRa device, used for RX and TX done 
-#define BUZZER -1                               //pin for buzzer, on when logic high, set to -1 to turn buzzer off
-#define SDCS 4                                  //CS pin for SD card 
 
 #define LORA_DEVICE DEVICE_SX1278               //we need to define the device we are using
 
-#define DisplayAddress 0x3F                     //I2C address of PCF8574 on display, common options are 0x27 and 0x3F
 
-//*******  Setup LoRa Parameters Here ! ***************
+//*******  Setup Test Parameters Here ! ***************
 
 //LoRa Modem Parameters
 const uint32_t Frequency = 434000000;           //frequency of transmissions in hertz
 const uint32_t Offset = 0;                      //offset frequency for calibration purposes
 
-const uint8_t Bandwidth = LORA_BW_125;          //LoRa bandwidth
-const uint8_t SpreadingFactor = LORA_SF7;       //LoRa spreading factor
-const uint8_t CodeRate = LORA_CR_4_5;           //LoRa coding rate
-const uint8_t Optimisation = LDRO_AUTO;         //low data rate optimisation setting, normally set to auto
-
-const uint32_t RXtimeoutmS = 60000;             //RXtimeout in mS
-const uint8_t RXBUFFER_SIZE = 128;              //RX buffer size
-
-const uint8_t LCD20x4_I2C_ADDRESS = 0x3F;       //I2C address of PCF8574 controller on LCD, could be 0x27 or 0x3F
+const uint32_t tone_lengthmS = 3000;            //tone length in mS
+const uint16_t tone_frequencyHz = 500;          //tone frequency in hertz
+const float adjustfreq = 0.9;                   //adjustment to tone frequency
+const uint16_t deviation = 10000;               //frequency deviation of RF output carrier
+const int8_t transmit_power = 2;                //LoRa transmit power in dBm
+const uint32_t transmission_delaymS = 2000;     //mS delay between transmissions
--- examples/SX127x_examples/Diagnostic_and_Test/AntennaTesting/111_RSSI_Tone_Meter_SSD1306/Settings.h
+++ examples/SX127x_examples/Diagnostic_and_Test/AntennaTesting/111_RSSI_Tone_Meter_SSD1306/Settings.h
@@ -7,21 +7,19 @@
 
 //*******  Setup hardware pin definitions here ! ***************
 
-//These are the pin definitions for one of my own boards based on a ATMega328 ProMini
-//be sure to change the definitions to match your own setup.
+//These are the pin definitions for one of my own boards, the Easy Pro Mini,
+//be sure to change the definitions to match your own setup. Some pins such as DIO1,
+//DIO2, BUZZER may not be in used by this sketch so they do not need to be
+//connected and should be set to -1.
 
 #define NSS 10                                  //select pin on LoRa device
 #define NRESET 9                                //reset pin on LoRa device
 #define LED1 8                                  //on board LED, high for on
 #define DIO0 3                                  //DIO0 pin on LoRa device, used for RX and TX done 
-#define BUZZER -1                               //pin for buzzer, on when logic high, set to -1 to turn buzzer off
-#define SDCS 4                                  //CS pin for SD card 
 
 #define LORA_DEVICE DEVICE_SX1278               //we need to define the device we are using
 
-#define DisplayAddress 0x3F                     //I2C address of PCF8574 on display, common options are 0x27 and 0x3F
-
-//*******  Setup LoRa Parameters Here ! ***************
+//*******  Setup Test Parameters Here ! ***************
 
 //LoRa Modem Parameters
 const uint32_t Frequency = 434000000;           //frequency of transmissions in hertz
@@ -31,8 +29,3 @@ const uint8_t Bandwidth = LORA_BW_125;          //LoRa bandwidth
 const uint8_t SpreadingFactor = LORA_SF7;       //LoRa spreading factor
 const uint8_t CodeRate = LORA_CR_4_5;           //LoRa coding rate
 const uint8_t Optimisation = LDRO_AUTO;         //low data rate optimisation setting, normally set to auto
-
-const uint32_t RXtimeoutmS = 60000;             //RXtimeout in mS
-const uint8_t RXBUFFER_SIZE = 128;              //RX buffer size
-
-const uint8_t LCD20x4_I2C_ADDRESS = 0x3F;       //I2C address of PCF8574 controller on LCD, could be 0x27 or 0x3F
