32526e564305dc4ebceef0fe813e7f796bd59bb1
StuartsProjects <stuart@stuartsprojects.com>
2026-01-08 13:48:34 +0000

    Update SX127X basics examples

--- examples/SX127x_examples/Basics/3_LoRa_TransmitterIRQ/3_LoRa_TransmitterIRQ.ino
+++ examples/SX127x_examples/Basics/3_LoRa_TransmitterIRQ/3_LoRa_TransmitterIRQ.ino
@@ -1,51 +1,107 @@
 /*******************************************************************************************************
-  Programs for Arduino - Copyright of the author Stuart Robinson - 23/03/22
+  Programs for Arduino - Copyright of the author Stuart Robinson - 03/01/26
 
   This program is supplied as is, it is up to the user of the program to decide if the program is
   suitable for the intended purpose and free from errors.
 *******************************************************************************************************/
 
+/******************************************************************************************************
+  Program Operation - This is a simple LoRa test transmitter. A packet containing ASCII text is sent
+  according to the frequency and LoRa settings specified at the beginning of the sketch. The GPIO pins
+  used to access the LoRa device need to be defined also. The SPI pins for the Arduino board used must
+  be connected to the LoRa device. This program does not need the DIO0 pin on the LoRa device connected.
 
-/*******************************************************************************************************
-  Program Operation - This is a minimum setup LoRa test transmitter. A packet containing the ASCII text
-  "Hello World 1234567890" is sent using the frequency and LoRa settings specified in the LT.setupLoRa()
-  command. The pins to access the lora device need to be defined at the top of the program also.
+  The details of the packet sent and any errors are shown on the Serial Monitor, together with the transmit
+  power used and the packet length. The matching receive program, '4_LoRa_Receiver' or '4_LoRa_ReceiverIRQ'
+  can be used to check the packets are being sent correctly, the frequency and LoRa settings used must be
+  the same for the Transmit and Receive programs. Sample Serial Monitor output;
 
-  This program does not need the DIO0 pin on the LoRa device connected.
-
-  The details of the packet sent and any errors are shown on the Arduino IDE Serial Monitor, together with
-  the transmit power used and the packet length. The matching receiver program, '4_LoRa_Receiver' can be used
-  to check the packets are being sent correctly, the frequency and LoRa settings (in the LT.setupLoRa()
-  commands) must be the same for the transmitter and receiver programs. Sample Serial Monitor output;
-
-  10dBm Packet> Hello World 1234567890*  BytesSent,23  PacketsSent,6
-
-  For an example of a more detailed configuration for a transmitter, see program 103_LoRa_Transmitter.
+  10dBm Packet> LoRa 00006  BytesSent,11  PacketsSent,6
 
   Serial monitor baud rate is set at 9600
 *******************************************************************************************************/
 
-#include <SPI.h>                                //the lora device is SPI based so load the SPI library                                         
+
+#include <SPI.h>                                //the LoRa device is SPI based so load the SPI library                                         
 #include <SX127XLT.h>                           //include the appropriate library  
 
 SX127XLT LT;                                    //create a library class instance called LT
 
 #define NSS 10                                  //select pin on LoRa device
 #define NRESET 9                                //reset pin on LoRa device
+#define DIO0 3                                  //DIO0 pin on LoRa device, used for sensing RX and TX done 
 #define LORA_DEVICE DEVICE_SX1278               //we need to define the device we are using
 #define TXpower 10                              //LoRa transmit power in dBm
 
 uint8_t TXPacketL;
-uint32_t TXPacketCount;
+uint32_t TXPacketCount = 0;
+uint16_t PacketCount = 0;
 
-uint8_t buff[] = "Hello World 1234567890";      //the message to send
+uint8_t buff[] = "LoRa 00000";                  //the message to send
+
+
+void loop()
+{
+  PacketCount++;
+
+  buff[5] = PacketCount / 10000 + '0';
+  buff[6] = ((PacketCount % 10000) / 1000) + '0';
+  buff[7] = ((PacketCount % 1000) / 100) + '0';
+  buff[8] = ((PacketCount % 100) / 10) + '0';
+  buff[9] = PacketCount % 10 + '0' ;
+
+  Serial.print(TXpower);                                       //print the transmit power defined
+  Serial.print(F("dBm > "));
+  Serial.flush();
+
+  TXPacketL = sizeof(buff);                                    //set TXPacketL to length of array
+
+  LT.printASCIIPacket(buff, TXPacketL);                        //print the buffer (the sent packet) as ASCII
+
+  if (LT.transmitIRQ(buff, TXPacketL, 10000, TXpower, WAIT_TX))   //will return packet length sent if OK, otherwise 0 if transmit error
+  {
+    TXPacketCount++;
+    packet_is_OK();
+  }
+  else
+  {
+    packet_is_Error();                                         //transmit packet returned 0, there was an error
+  }
+
+  delay(500);                                                  //have a delay between packets
+  Serial.println();
+}
+
+
+void packet_is_OK()
+{
+  //if here packet has been sent OK
+  Serial.print(F("  Bytes,"));
+  Serial.print(TXPacketL);                             //print transmitted packet length
+  Serial.print(F("  TX,"));
+  Serial.print(TXPacketCount);                         //print total of packets sent OK
+}
+
+
+void packet_is_Error()
+{
+  //if here there was an error transmitting packet
+  uint16_t IRQStatus;
+  IRQStatus = LT.readIrqStatus();                      //read the the interrupt register
+  Serial.print(F(" SendError,"));
+  Serial.print(F("Length,"));
+  Serial.print(TXPacketL);                             //print transmitted packet length
+  Serial.print(F(",IRQreg,"));
+  Serial.print(IRQStatus, HEX);                        //print IRQ status
+  LT.printIrqStatus();                                 //prints the text of which IRQs set
+}
 
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println();
-  Serial.println(F("3_LoRa_Transmitter Starting"));
+  Serial.println(F("3_LoRa_TransmitterIRQ Starting"));
 
   SPI.begin();
 
@@ -64,55 +120,4 @@ void setup()
 
   Serial.print(F("Transmitter ready"));
   Serial.println();
-}
-
-
-void loop()
-{
-  Serial.print(TXpower);                                       //print the transmit power defined
-  Serial.print(F("dBm "));
-  Serial.print(F("Packet> "));
-  Serial.flush();
-
-  TXPacketL = sizeof(buff);                                    //set TXPacketL to length of array
-  buff[TXPacketL - 1] = '*';                                   //replace null character at buffer end so its visible on receiver
-
-  LT.printASCIIPacket(buff, TXPacketL);                        //print the buffer (the sent packet) as ASCII
-
-  if (LT.transmitIRQ(buff, TXPacketL, 10000, TXpower, WAIT_TX))   //will return packet length sent if OK, otherwise 0 if transmit error
-  {
-    TXPacketCount++;
-    packet_is_OK();
-  }
-  else
-  {
-    packet_is_Error();                                         //transmit packet returned 0, there was an error
-  }
-
-  Serial.println();
-  delay(1000);                                                 //have a delay between packets
-}
-
-
-void packet_is_OK()
-{
-  //if here packet has been sent OK
-  Serial.print(F("  BytesSent,"));
-  Serial.print(TXPacketL);                             //print transmitted packet length
-  Serial.print(F("  PacketsSent,"));
-  Serial.print(TXPacketCount);                         //print total of packets sent OK
-}
-
-
-void packet_is_Error()
-{
-  //if here there was an error transmitting packet
-  uint16_t IRQStatus;
-  IRQStatus = LT.readIrqStatus();                      //read the the interrupt register
-  Serial.print(F(" SendError,"));
-  Serial.print(F("Length,"));
-  Serial.print(TXPacketL);                             //print transmitted packet length
-  Serial.print(F(",IRQreg,"));
-  Serial.print(IRQStatus, HEX);                        //print IRQ status
-  LT.printIrqStatus();                                 //prints the text of which IRQs set
 }
--- examples/SX127x_examples/Basics/4_LoRa_ReceiverIRQ/4_LoRa_ReceiverIRQ.ino
+++ examples/SX127x_examples/Basics/4_LoRa_ReceiverIRQ/4_LoRa_ReceiverIRQ.ino
@@ -1,37 +1,32 @@
 /*******************************************************************************************************
-  Programs for Arduino - Copyright of the author Stuart Robinson - 23/03/22
+  Programs for Arduino - Copyright of the author Stuart Robinson - 03/01/26
 
   This program is supplied as is, it is up to the user of the program to decide if the program is
   suitable for the intended purpose and free from errors.
 *******************************************************************************************************/
 
 /*******************************************************************************************************
-  Program Operation - This is a minimum setup LoRa test receiver. The program listens for incoming packets
-  using the frequency and LoRa settings in the LT.setupLoRa() command. The pins to access the lora device
-  need to be defined at the top of the program also.
-
+  Program Operation - The program listens for incoming packets using the frequency and LoRa settings
+  specified at the beginning of the sketch. The GPIO pins used to access the LoRa device need to be
+  defined also. The SPI pins for the Arduino board used must be connected to the LoRa device.
   This program does not need the DIO0 pin on the LoRa device connected.
 
-  There is a printout on the Arduino IDE serial monitor of the valid packets received, the packet is assumed
-  to be in ASCII printable text, if it's not ASCII text characters from 0x20 to 0x7F, expect weird things to
-  happen on the Serial Monitor. Sample serial monitor output;
+  There is a printout of the valid packets received, the packet is assumed to be in ASCII printable text,
+  if its not ASCII text characters from 0x20 to 0x7F, expect weird things to happen on the Serial Monitor.
+  The LED will flash for each packet received.
 
-  8s  Hello World 1234567890*,RSSI,-44dBm,SNR,9dB,Length,23,Packets,7,Errors,0,IRQreg,50
+  Sample serial monitor output;
 
-  If there is a packet error it might look like this, which is showing a CRC error;
+  3s  LoRa 00005,RSSI,-39dBm,SNR,13dB,Length,11,Packets,5,Errors,0,IRQreg,50
 
-  137s PacketError,RSSI,-89dBm,SNR,-8dB,Length,23,Packets,37,Errors,2,IRQreg,70,IRQ_HEADER_VALID,IRQ_CRC_ERROR,IRQ_RX_DONE
+  If there is a packet error it might look like this, which is showing a CRC error in the received packet,
 
-  If there are no packets received in a 10 second period then you should see a message like this;
-
-  112s RXTimeout
-
-  For an example of a more detailed configuration for a receiver, see program 104_LoRa_Receiver.
+  1189s PacketError,RSSI,-111dBm,SNR,-12dB,Length,0,Packets,26,Errors,1,IRQreg,70,IRQ_HEADER_VALID,IRQ_CRC_ERROR,IRQ_RX_DONE
 
   Serial monitor baud rate is set at 9600.
 *******************************************************************************************************/
 
-#include <SPI.h>                                //the lora device is SPI based so load the SPI library
+#include <SPI.h>                                //the LoRa device is SPI based so load the SPI library
 #include <SX127XLT.h>                           //include the appropriate library   
 
 SX127XLT LT;                                    //create a library class instance called LT
@@ -51,34 +46,6 @@ int16_t PacketRSSI;                             //stores RSSI of received packet
 int8_t  PacketSNR;                              //stores signal to noise ratio (SNR) of received packet
 
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println();
-  Serial.println(F("4_LoRa_Receiver Starting"));
-  Serial.println();
-
-  SPI.begin();
-
-  if (LT.begin(NSS, NRESET, LORA_DEVICE))
-  {
-    Serial.println(F("LoRa Device found"));
-    delay(1000);
-  }
-  else
-  {
-    Serial.println(F("No device responding"));
-    while (1);
-  }
-
-  LT.setupLoRa(434000000, 0, LORA_SF7, LORA_BW_125, LORA_CR_4_5, LDRO_AUTO);   //configure frequency and LoRa settings
-
-  Serial.print(F("Receiver ready - RXBUFFER_SIZE "));
-  Serial.println(RXBUFFER_SIZE);
-  Serial.println();
-}
-
-
 void loop()
 {
   RXPacketL = LT.receiveIRQ(RXBUFFER, RXBUFFER_SIZE, 60000, WAIT_RX); //wait for a packet to arrive with 60seconds (60000mS) timeout
@@ -86,7 +53,7 @@ void loop()
   PacketRSSI = LT.readPacketRSSI();              //read the received packets RSSI value
   PacketSNR = LT.readPacketSNR();                //read the received packets SNR value
 
-  if (RXPacketL == 0)                            //if the LT.receive() function detects an error RXpacketL is 0
+  if (RXPacketL == 0)                            //if the LT.receiveIRQ() function detects an error RXpacketL is 0
   {
     packet_is_Error();
   }
@@ -107,10 +74,10 @@ void packet_is_OK()
   IRQStatus = LT.readIrqStatus();                  //read the LoRa device IRQ status register
   printElapsedTime();                              //print elapsed time to Serial Monitor
 
-  Serial.print(F("  "));
+  Serial.print(F(" "));
   LT.printASCIIPacket(RXBUFFER, RXPacketL);        //print the packet as ASCII characters
 
-  Serial.print(F(",RSSI,"));
+  Serial.print(F(" RSSI,"));
   Serial.print(PacketRSSI);
   Serial.print(F("dBm,SNR,"));
   Serial.print(PacketSNR);
@@ -163,4 +130,32 @@ void printElapsedTime()
   seconds = millis() / 1000;
   Serial.print(seconds, 0);
   Serial.print(F("s"));
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("4_LoRa_ReceiverIRQ Starting"));
+  Serial.println();
+
+  SPI.begin();
+
+  if (LT.begin(NSS, NRESET, LORA_DEVICE))
+  {
+    Serial.println(F("LoRa Device found"));
+    delay(1000);
+  }
+  else
+  {
+    Serial.println(F("No device responding"));
+    while (1);
+  }
+
+  LT.setupLoRa(434000000, 0, LORA_SF7, LORA_BW_125, LORA_CR_4_5, LDRO_AUTO);   //configure frequency and LoRa settings
+
+  Serial.print(F("Receiver ready - RXBUFFER_SIZE "));
+  Serial.println(RXBUFFER_SIZE);
+  Serial.println();
 }
